8f44a5b2abeb68e3df4c1f387927b1619b0b781b
striker.sh <strikersh@disroot.org>
2021-05-25 21:22:34 +0200

    Allow W^X needed for mmap

--- src/codegen_allocator.c
+++ src/codegen_allocator.c
@@ -31,7 +31,7 @@ void codegen_allocator_init()
 #if defined WIN32 || defined _WIN32 || defined _WIN32
         mem_block_alloc = VirtualAlloc(NULL, MEM_BLOCK_NR * MEM_BLOCK_SIZE, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 #else
-        mem_block_alloc = mmap(0, MEM_BLOCK_NR * MEM_BLOCK_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANON|MAP_PRIVATE, 0, 0);
+        mem_block_alloc = mmap(0, MEM_BLOCK_NR * MEM_BLOCK_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANON|MAP_PRIVATE, -1, 0);
 #endif
 
         for (c = 0; c < MEM_BLOCK_NR; c++)
