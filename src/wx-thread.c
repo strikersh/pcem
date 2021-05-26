c24c8ceaf622cb0462dee1602eef7696b2b422ad
striker.sh <strikersh@disroot.org>
2021-05-26 12:34:03 +0200

    Use clock_gettime for both linux and bsd

--- src/wx-thread.c
+++ src/wx-thread.c
@@ -171,7 +171,7 @@ int thread_wait_event(event_t *handle, int timeout)
 	event_pthread_t *event = (event_pthread_t *)handle;
 	struct timespec abstime;
 
-#ifdef __linux__
+#ifdef __unix__
 	clock_gettime(CLOCK_REALTIME, &abstime);
 #else
         struct timeval now;
